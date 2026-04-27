a8e1c9e83495e858ea7eef19a6dfb7a1fbf625b6
jefin-s <jefin.s786@gmail.com>
2026-04-27 15:18:35 +0530

    create ci/cd pipleline w

--- src/features/admin/DeleteConfirmModal.jsx
+++ src/features/admin/DeleteConfirmModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { AlertTriangle, Loader2, X } from 'lucide-react';
 
 const DeleteConfirmModal = ({ isOpen, onClose, onConfirm, itemName, isLoading }) => {
--- src/features/admin/DepartmentManagement.jsx
+++ src/features/admin/DepartmentManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import  { useState, useEffect, useCallback } from 'react';
 import ManagementTable from './ManagementTable';
 import GenericModal from './GenericModal';
 import DeleteConfirmModal from './DeleteConfirmModal';
